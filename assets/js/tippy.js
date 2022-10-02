9282e6c6b10348f07815e8f3b57c909d219d8093
sundei <76597738+smhman@users.noreply.github.com>
2022-10-02 22:27:14 +0300

    Create tippy.js

---
+++ assets/js/tippy.js
@@ -0,0 +1,35 @@
+tippy('#headAvatar', {
+    content: '<p class="smallText opacity-80 ml-1 mr-1">Made in <b>Lietuvos Respublika</b></p>',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws',
+    inertia: true,
+    interactive: false,
+    allowHTML: true,
+    followCursor: false,
+    arrow: false
+});
+
+tippy('#switchModeButton', {
+    content: '<p class="smallText opacity-80 ml-1 mr-1">Toggle <b>Dark Mode</b></p>',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws',
+    inertia: true,
+    interactive: false,
+    allowHTML: true,
+    followCursor: false,
+    arrow: false
+});
+
+tippy('#discordStatus', {
+    content: '<p class="smallText opacity-80 ml-1 mr-1">Discord Status</p>',
+    animation: 'scale',
+    placement: 'right',
+    theme: 'ws',
+    inertia: true,
+    interactive: false,
+    allowHTML: true,
+    followCursor: false,
+    arrow: false
+});
